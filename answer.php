c3df25c9dc437ac5ccf740e6fb3e9d03824f5428
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-03-17 17:18:43 +0000

    init

--- answer.php
+++ answer.php
@@ -1,20 +1,15 @@
 <!DOCTYPE html>
-<!-- ICS2O-Unit2-01-PHP -->
-<html lang="en-ca">
+<html>
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="The Area and Perimeter Program" />
+  <meta name="description" content="The Area and Perimeter program,in PHP" />
   <meta name="keywords" content="mths, icd2o" />
-  <meta name="author" content="Shanea Jaromay" />
+  <meta name="author" content="Ain Jeong" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link
-    rel="stylesheet"
-    href="https://code.getmdl.io/1.3.0/material.blue_grey-yellow.min.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue_grey-blue.min.css" />
   <link rel="apple-touch-icon" sizes="180x180" href="./images/apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon-16x16.png" />
@@ -24,7 +19,6 @@
 
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  <script src="./js/script.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
@@ -35,18 +29,14 @@
       <div class="right-image">
         <img src="./images/area_and_perimeter (1).png" alt="math image" />
       </div>
-      <div class="page-content">The area and perimeter is ...</div>
+      <div class="page-content-php">
+        <?php echo '<p>The area is: ' . (5 * 3) . 'cm²</p>'; ?>
+        <?php echo '<p>The perimeter is: ' . (2 * 5 + 2 * 3) . 'cm</p>'; ?>
+      </div>
       <br />
-      <!-- Simple Textfield for integers-->
-      <form action="answer.php" method="GET">
-        <br />
-        <!-- Accent-colored raised button with ripple -->
-        <button
-          class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-          onclick="doMathClicked()"
-          type="button">
-          Do math
-        </button>
+      <div class="page-content-answer">
+        <a href="./index.php">Return ...</a>
+      </div>
     </main>
   </div>
 </body>
